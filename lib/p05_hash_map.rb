b586e44575c58f4d1ede557529cbded4ffb67b74
Taihyung(Peter) Kim <kim.taihyung@gmail.com>
2023-10-10 18:55:05 -0400

    define HashMap using doubly LinkedList

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -1,6 +1,7 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,27 +10,47 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    resize! if count == num_buckets
+    bucket = bucket(key)
+    if include?(key)
+      bucket.update(key, val)
+    else
+      bucket.append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    bucket(key).remove(key)
+    @count -= 1
   end
 
-  def each
+  def each(&prc)
+    @store.each do |bucket|
+      unless bucket.empty?
+        bucket.each do |node|
+          prc.call(node.key, node.val)
+        end
+      end 
+    end
+    self
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
   
   alias_method :[], :get
   alias_method :[]=, :set
@@ -41,9 +62,20 @@ class HashMap
   end
 
   def resize!
+    @count = 0
+    elements = @store.flatten
+    @store = Array.new(2 * num_buckets) { LinkedList.new }
+    elements.each do |list|
+      unless list.empty?
+        list.each do |node|
+          set(node.key, node.val)
+        end
+      end
+    end
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
