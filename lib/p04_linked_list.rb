519138d51b421b34487926341b40006e19af1c3f
Taihyung(Peter) Kim <kim.taihyung@gmail.com>
2023-10-10 18:25:05 -0400

    define doubly linked list

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -16,12 +17,20 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    prev.next = self.next
+    self.next.prev = prev
   end
 
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new(nil, nil)
+    @tail = Node.new(nil, nil)
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -30,34 +39,64 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    self.each do |node|
+      return node.val if node.key == key
+    end
+    nil
   end
 
   def include?(key)
+    self.each do |node|
+      return true if node.key == key
+    end
+    false
   end
 
   def append(key, val)
+    prev_node = @tail.prev
+    new_node = Node.new(key, val)
+    
+    prev_node.next = new_node
+    new_node.prev = prev_node
+    new_node.next = @tail
+    @tail.prev = new_node
   end
 
   def update(key, val)
+    self.each do |node|
+      node.val = val if node.key == key
+    end
   end
 
   def remove(key)
+    self.each do |node|
+      node.remove if node.key == key
+    end
   end
 
-  def each
+  def each(&prc)
+    current = first
+    while current != @tail
+      prc.call(current)
+      current = current.next
+    end
+    self
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
+  end
 end
