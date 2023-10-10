5ffc0b3156a0ede8eb3d39c403f552654502d42c
Taihyung(Peter) Kim <kim.taihyung@gmail.com>
2023-10-10 17:46:54 -0400

    define hashing argo for Array, String and Hash

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,16 @@ end
 
 class Array
   def hash
+    val = 0
+    self.each_with_index { |el, idx| val += el.hash ^ idx.hash }
+    val
   end
 end
 
 class String
   def hash
+    chars = self.split('').map(&:ord)
+    chars.hash
   end
 end
 
@@ -16,6 +21,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort.hash
   end
 end
