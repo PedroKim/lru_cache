4a82dbe18c9eca5a5e5089ed9de271d1e1b6fe7c
Taihyung(Peter) Kim <kim.taihyung@gmail.com>
2023-10-11 16:37:48 -0400

    define DynamicArray

--- lib/p07_dynamic_array_bonus.rb
+++ lib/p07_dynamic_array_bonus.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class StaticArray
   attr_reader :store
 
@@ -27,6 +29,7 @@ class StaticArray
 end
 
 class DynamicArray
+  include Enumerable
   attr_accessor :count
   
   def initialize(capacity = 8)
@@ -35,9 +38,16 @@ class DynamicArray
   end
 
   def [](i)
+    i = count + i if i < 0
+    return nil if i < 0
+    @store[i]
   end
 
   def []=(i, val)
+    i = count + i if i < 0
+    resize! if i > capacity - 1
+    @count += 1 if @store[i].nil?
+    @store[i] = val
   end
 
   def capacity
@@ -45,27 +55,70 @@ class DynamicArray
   end
 
   def include?(val)
+    self.each do |val|
+      return true if val == val
+    end
+    false
   end
 
   def push(val)
+    resize! if count == capacity
+    @store[count] = val
+    @count += 1 unless val.nil?
   end
 
   def unshift(val)
+    @count += 1
+    resize! if count == capacity
+    i = count - 1
+    while i > 0
+      @store[i] = @store[i - 1]
+      i -= 1
+    end
+    @store[0] = val
   end
 
   def pop
+    return nil if count == 0
+
+    val = @store[count - 1]
+    @store[count - 1] = nil
+    @count -= 1
+
+    return val
   end
 
   def shift
+    return nil if count == 0
+    val = @store[0]
+
+    i = 1
+    while i < capacity
+      @store[i - 1] = @store[i]
+      i += 1
+    end
+
+    @count -= 1 unless val.nil?
+    @store[capacity - 1] = nil
+
+    return val
   end
 
   def first
+    self[0]
   end
 
   def last
+    self[count - 1]
   end
 
-  def each
+  def each(&prc)
+    i = 0
+    while i < count
+      prc.call(self[i])
+      i += 1
+    end
+    self
   end
 
   def to_s
@@ -74,7 +127,11 @@ class DynamicArray
 
   def ==(other)
     return false unless [Array, DynamicArray].include?(other.class)
-    # Your code here
+    (0...capacity).each do |i|
+      return false if self[i] != other[i]
+    end
+
+    true
   end
 
   alias_method :<<, :push
@@ -83,5 +140,9 @@ class DynamicArray
   private
 
   def resize!
+    @count = 0
+    elements = @store.store.dup
+    @store = StaticArray.new(2 * capacity)
+    (0...elements.length).each { |i| self[i] = elements[i] }
   end
 end
